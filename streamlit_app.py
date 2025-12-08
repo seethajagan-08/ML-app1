9edb2372345e410193b22f176d5d7a4fc3fdcf09
Seetha Jagan <regd.asseetha@gmail.com>
2025-12-08 10:03:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import pandas as pd
+'''import pandas as pd
 import numpy as np
 import pickle
 # from sklearn.linear_model import LinearRegression
@@ -34,6 +34,49 @@ def main():
         st.text("Lets Learn")
         st.text("Built with Streamlit")
         
+if __name__=='__main__':
+    main()'''
+
+
+
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st 
+
+# from PIL import Image
+
+
+pickle_in = open("marks.pkl","rb")
+lin_model=pickle.load(pickle_in)
+
+
+def welcome():
+    return "Welcome All"
+
+def predict_authentication(time_study):   
+    prediction=mlr.predict([[time_study]])
+    return prediction
+
+
+
+def main():
+    st.title("Marks Prediction")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Marks Prediction ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    time_study = st.number_input("Time spent on study(in hrs)",min_value=0, step=1)
+    result=""
+    if st.button("Predict my Marks"):
+        result=predict_authentication(time_study)
+    st.success('You may get Marks:{}, if you study {}hours'.format(result, time_study))
+    if st.button("About"):
+        st.text("Lets Learn")
+        st.text("Built with Streamlit")
+
 if __name__=='__main__':
     main()
     
