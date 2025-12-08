1d8b8641e4636f0beddaf31c61103e6b8eb4e924
Seetha Jagan <regd.asseetha@gmail.com>
2025-12-08 09:33:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
+import pandas as pd
+import numpy as np
+import pickle
 import streamlit as st
 
 st.title('Marks Prediction')
 st.info('Predicting Marks for students based on their time spent')
-st.write('Hello world!')
+# st.write('Hello world!')
+
+pickle_in = open("marks_pred.pkl", "rb")
+lin_mod = pickle.load(pickle_in)
+
+def predict_authntication('time_study'):
+  prediction=lin_mod([[time_study]])
+  return prediction
